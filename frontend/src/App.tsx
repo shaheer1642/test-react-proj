12b3b68b007b061275c18c8a1bc8a36a0ebc7242
shaheer1642 <shaheer1642@gmail.com>
2025-08-07 05:52:47 +0500

    feat: counter example

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [counter, setCounter] = useState(1)
+  const [counter2, setCounter2] = useState(1)
+  const [counter3, setCounter3] = useState(1)
+  const [counter4, setCounter4] = useState(1)
+  const [counter5, setCounter5] = useState(1)
+  const [counter6, setCounter6] = useState(1)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div style={{ padding: 10 }}>
+      <button onClick={() => setCounter(v => v + 1)}>Increase count</button>
+      <button onClick={() => setCounter2(v => v + 1)}>Increase count</button>
+      <button onClick={() => setCounter3(v => v + 1)}>Increase count</button>
+      <button onClick={() => setCounter4(v => v + 1)}>Increase count</button>
+      <button onClick={() => setCounter5(v => v + 1)}>Increase count</button>
+      <button onClick={() => setCounter6(v => v + 1)}>Increase count</button>
+      <p>{counter}</p>
+      <p>{counter2}</p>
+      <p>{counter3}</p>
+      <p>{counter4}</p>
+      <p>{counter5}</p>
+      <p>{counter6}</p>
+    </div>
   )
 }
 
